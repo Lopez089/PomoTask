35fec61b3a89fd608229a65e9caeef5f491d7587
LopezDev <aragonlopezjuan87@gmail.com>
2023-08-13 00:20:17 +0200

    first comint

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './boton';
+export * from './clock';
+export * from './nav';
+export * from './newTask';
+export * from './pomodoroState';
+export * from './tareas';
---
+++ src/containers/newTaskContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewTaskContainer = () => {
+  return (
+    <div>NewTaskContainer</div>
+  )
+}
